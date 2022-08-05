e861c58da4ddaedcc6f5238e29f62fb82d383ed6
Holdest White <zbsholdest@mail.ru>
2022-08-05 04:09:19 +0300

    adds

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'css-graphics-practice';
+  money = 10000;
+
+  flaskHeight = 800;
+  bitStep = 5000
+  steps = 20 + 0.5;
+
+  pixelToMoney =  this.flaskHeight / (this.steps * this.bitStep);
+  currentHeight = this.flaskHeight / 2;
+
+
+  private querySubscription: Subscription
+
+
+  constructor(public activateRoute: ActivatedRoute){
+
+
+
+    this.querySubscription = activateRoute.queryParams.subscribe(
+      (queryParam: any) => {
+        this.money = queryParam['money']
+        this.currentHeight = this.pixelToMoney * this.money;
+        console.log(this.currentHeight)
+      }
+    )
+
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from "@angular/router";
+
+
+const appRoutes: Routes =[
+  { path: '', component: AppComponent },
+  { path: 'set/:money', component: AppComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]

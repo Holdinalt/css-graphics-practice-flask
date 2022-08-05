f797a95552c781fe13163863366468cfffb3e745
Holdest White <zbsholdest@mail.ru>
2022-08-05 17:49:43 +0300

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {Subscription} from "rxjs";
+
+class reward{
+  constructor(public price: number, public value: string) {
+  }
+}
 
 @Component({
   selector: 'app-root',
@@ -12,11 +17,29 @@ export class AppComponent{
   money = 10000;
 
   flaskHeight = 800;
-  bitStep = 5000
-  steps = 20 + 0.5;
 
-  pixelToMoney =  this.flaskHeight / (this.steps * this.bitStep);
-  currentHeight = this.flaskHeight / 2;
+
+  fontHeight = 37;
+  currentHeight = 200;
+  marginBottom = 10;
+
+  partHeight = 100;
+
+  data: reward[] = [
+    new reward(5000, 'ВЕЧЕР ФИЛЬМОВ УЖАСОВ'),
+    new reward(10000, 'ДЕНЬ ИГРЫ В LOL НА КОНТРОЛЛЕРЕ'),
+    new reward(15000, 'ИГРЫ СО ЗРИТЕЛЯМИ'),
+    new reward(20000, 'ПРОБУЮ СУПЕР ОСТРУЮ ЕДУ'),
+    new reward(25000, 'КАРАОКЕ (ПЕСНИ НА ВЫБОР ЧАТА)'),
+    new reward(30000, 'СТРИМ ПО ДОТА 2'),
+    new reward(35000, 'IRL СТРИМ В ПАРКЕ'),
+    new reward(40000, 'БУХЛОСТРИМ LOL И APEX'),
+    new reward(45000, 'СМОТРИМ АНИМЕ (НА ВЫБОР ЧАТА)'),
+    new reward(50000, 'РОЗЫГРЫШ 50 СКИНОВ'),
+    new reward(60000, 'СБРИВАЮ БОРОДУ (ОСТАВЛЯЮ УСЫ)'),
+    new reward(75000, 'КРАШУ ВОЛОСЫ (ЦВЕТ ВЫБИРАЕТ ЧАТ)'),
+    new reward(100000, '!GENERAL_HS ЧЕЛЛЕНЖ')
+  ]
 
 
   private querySubscription: Subscription
@@ -24,16 +47,71 @@ export class AppComponent{
 
   constructor(public activateRoute: ActivatedRoute){
 
+    this.marginBottom = this.calcMargin(
+      this.flaskHeight - (this.data.length * this.fontHeight),
+      this.data.length
+    )
 
+    this.partHeight = this.marginBottom + this.fontHeight;
 
     this.querySubscription = activateRoute.queryParams.subscribe(
       (queryParam: any) => {
         this.money = queryParam['money']
-        this.currentHeight = this.pixelToMoney * this.money;
-        console.log(this.currentHeight)
+
+        let last_id = this.findIdLastNotReached(this.money, this.data);
+
+        if(last_id >= this.data.length){
+          this.money = this.data[this.data.length - 1].price
+        }
+
+        let next_percent;
+
+        if(last_id == 0){
+
+          if(this.money == 0){
+            next_percent = 0;
+          } else {
+            next_percent = this.money / this.data[last_id].price
+          }
+
+        }else{
+
+          if(this.money - this.data[last_id - 1].price == 0){
+            next_percent = 0;
+          } else {
+            next_percent = (this.money - this.data[last_id - 1].price) / (this.data[last_id].price - this.data[last_id - 1].price)
+          }
+
+        }
+
+
+        this.currentHeight = this.calcCurrentHeight(last_id, next_percent);
+
       }
     )
+  }
 
+  calcMargin(freeSpace: number, partsNumber: number){
+    return freeSpace / (partsNumber + 1)
+  }
+
+
+  findIdLastNotReached(score: number, rewards: reward[]): number{
+    for(let i = 0; i < rewards.length; i++){
+      if(rewards[i].price > score){
+        return i;
+      }
+    }
+
+    return rewards.length;
+  }
+
+  calcCurrentHeight(partsDone: number, percentNextDone: number){
+    if(partsDone == 0){
+      return (this.marginBottom + this.fontHeight / 2) * percentNextDone;
+    }else{
+      return (partsDone - 1) * this.partHeight + percentNextDone * this.partHeight + (this.marginBottom + this.fontHeight / 2);
+    }
 
   }
 }
